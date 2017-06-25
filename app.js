6010c46e2d0b291183cd778285c7c1562c9e53ef
The Good Maxwell <maxwellraden@gmail.com>
2017-06-25 08:29:57 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let board = [
+	[],
+	[],
+	[]
+];
+
+let victoryConditions = [
+  [[0,0], [1,1], [2,2]], // Down-Right diagonal
+  [[0,0], [1,0], [2,0]], // Top row
+];
+let victoryConditions = {
+
+}

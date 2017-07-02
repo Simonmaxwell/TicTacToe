e29f5ec54f6aeac304ff56b4ddc228ca647f7085
The Good Maxwell <maxwellraden@gmail.com>
2017-07-02 16:24:46 -0500

    added click functions to table cells and value parsing.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+	$(".cell").click(function() {
+		let row = parseInt($(this).attr("data")[0]);
+		console.log(row);
+		console.log($(this).attr("data"));
+	});
+});
+
+
+
+
+
+// game begins on turn x; any box clicked during a turn is given a value of turnname;
+// clicking a box trigges a check of the table data, if no victory conditions are present the turn order advances;
+// When a victory condition is present a winner is declared;
+
+
+
+
+
 let board = [
 	[],
 	[],
@@ -8,6 +28,3 @@ let victoryConditions = [
   [[0,0], [1,1], [2,2]], // Down-Right diagonal
   [[0,0], [1,0], [2,0]], // Top row
 ];
-let victoryConditions = {
-
-}
